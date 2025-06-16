a39a8daf16684fd1935e9b7872a8eb33d916fa69
yujianzhao <zhaoyujian@ucla.edu>
2025-06-16 13:51:36 -0400

    Solution to problem 3 in problem set 1.

--- PS1/src/p03d_poisson.py
+++ PS1/src/p03d_poisson.py
@@ -1,5 +1,6 @@
 import numpy as np
 import util
+import matplotlib.pyplot as plt
 
 from linear_model import LinearModel
 
@@ -19,6 +20,22 @@ def main(lr, train_path, eval_path, pred_path):
     # *** START CODE HERE ***
     # Fit a Poisson Regression model
     # Run on the validation set, and use np.savetxt to save outputs to pred_path
+    
+    m,n = x_train.shape
+    theta_0 = np.zeros(n)
+    clf = PoissonRegression(step_size=lr, max_iter=1000, eps=1e-5, theta_0=theta_0)
+    clf.fit(x_train, y_train)
+
+    x_valid, y_valid = util.load_dataset(eval_path, add_intercept=False)
+    y_pred = clf.predict(x_valid)
+    util.save_prediction(y_pred, pred_path, usePandas=False)
+
+    plt.plot(y_valid, y_pred, 'bx')
+    plt.plot(y_valid, y_valid, 'r', label='y=x')
+    plt.xlabel('true counts')
+    plt.ylabel('predict counts')
+    plt.legend()
+    plt.show()
     # *** END CODE HERE ***
 
 
@@ -39,6 +56,16 @@ class PoissonRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+        m,n = x.shape
+        
+        theta_old = self.theta
+        for it in range(self.max_iter):
+            h = np.exp(x @ theta_old)
+            self.theta = theta_old + self.step_size * x.T @ (y - h)
+            if np.linalg.norm(self.theta - theta_old, ord=1) < self.eps:
+                break
+            theta_old = self.theta
+            
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -51,4 +78,5 @@ class PoissonRegression(LinearModel):
             Floating-point prediction for each input, shape (m,).
         """
         # *** START CODE HERE ***
+        return np.exp(x @ self.theta)
         # *** END CODE HERE ***
